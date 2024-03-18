98376befb359f64b5c4d391b08ab097e426d0451
bibyru <158981581+bibyru@users.noreply.github.com>
2024-03-18 12:10:39 +0700

    Add files via upload

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMove : MonoBehaviour
 {
+    int dir;
 
+    void GoUp()
+    {
+
+    }
+
+    void GoDown()
+    {
+
+    }
+
+    void Update()
+    {
+        if (dir != 0)
+        {
+            if (dir == 1)
+            {
+                GoUp();
+            }
+            else
+            {
+                GoDown();
+            }
+        }
+    }
 }
